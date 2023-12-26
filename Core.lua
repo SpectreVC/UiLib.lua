71252f511814279cef1ec2356db07ecbc22bf075
SpectreVC <154760436+SpectreVC@users.noreply.github.com>
2023-12-26 10:49:06 +0200

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -1,5 +1,5 @@
 local player = game.Players.LocalPlayer
---Tab
+
 
 function CreateTab(params)
     local playerGui = player:WaitForChild("PlayerGui")
@@ -49,20 +49,17 @@ function CreateTab(params)
 end
 
 
--- Function to create a toggle
+
+
+
+
 function CreateToggle(params)
     local toggleButton = Instance.new("TextButton")
     toggleButton.Name = params.Name
     toggleButton.Parent = params.ParentTab
     toggleButton.Position = params.Position or UDim2.new(0, 0, 0, 0)
     toggleButton.Size = UDim2.new(0, 200, 0, 30)
-    toggleButton.BackgroundTransparency = 0.5
-    toggleButton.BackgroundColor3 = Color3.new(0, 0, 0) -- Set default background color
-
-    local uiCorner = Instance.new("UICorner")
-    uiCorner.CornerRadius = UDim.new(0, 8)
-    uiCorner.Parent = toggleButton
-
+    toggleButton.BackgroundTransparency = 1
     toggleButton.Text = params.Name
     toggleButton.TextSize = 24
     toggleButton.Font = Enum.Font.DenkOne
@@ -90,22 +87,17 @@ function CreateToggle(params)
         isToggled = not isToggled
         params.Callback(isToggled)
 
-        toggleButton.TextColor3 = isToggled and Color3.new(1, 0, 1) or Color3.new(1, 1, 1)
-        toggleButton.BackgroundColor3 = isToggled and Color3.new(0.5, 0, 0.8) or Color3.new(0, 0, 0)
+        toggleButton.TextColor3 = isToggled and Color3.new(0.5, 0, 0.8) or Color3.new(1, 1, 1)
+        toggleButton.BackgroundColor3 = isToggled and Color3.new(0.2, 0.2, 0.2) or Color3.new(0, 0, 0)
 
         toggleStates[params.Name] = isToggled
         writefile(filePath, game.HttpService:JSONEncode(toggleStates))
     end)
 
-    -- Set initial colors based on the initial state
-    toggleButton.TextColor3 = isToggled and Color3.new(1, 0, 1) or Color3.new(1, 1, 1)
-    toggleButton.BackgroundColor3 = isToggled and Color3.new(0.5, 0, 0.8) or Color3.new(0, 0, 0)
-
     return toggleButton
 end
 
 
--- TabToggle
 function CreateTabToggle(params)
     local screenGui = Instance.new("ScreenGui")
     screenGui.Parent = player.PlayerGui
@@ -116,16 +108,12 @@ function CreateTabToggle(params)
     toggleButton.AnchorPoint = Vector2.new(1, 0.5)
     toggleButton.Position = params.Position or UDim2.new(0.95, 0, 0.4, 0)
     toggleButton.Size = params.Size or UDim2.new(0, 80, 0, 40)
-    toggleButton.BackgroundTransparency = 1
-
-    local gradient = Instance.new("UIGradient")
-    gradient.Rotation = 90
-    gradient.Color = ColorSequence.new{
-        ColorSequenceKeypoint.new(0, Color3.fromRGB(40, 40, 40)),
-        ColorSequenceKeypoint.new(0.5, Color3.fromRGB(30, 30, 60)),
-        ColorSequenceKeypoint.new(1, Color3.fromRGB(30, 30, 70))
-    }
-    gradient.Parent = toggleButton
+    toggleButton.BackgroundTransparency = 0.5
+    toggleButton.BackgroundColor3 = Color3.new(0, 0, 0)
+    toggleButton.Text = "VC"
+    toggleButton.TextSize = 18
+    toggleButton.Font = Enum.Font.DenkOne
+    toggleButton.TextColor3 = Color3.new(1, 1, 1)
 
     local uiStroke = Instance.new("UIStroke")
     uiStroke.Thickness = 1
@@ -136,7 +124,7 @@ function CreateTabToggle(params)
     uiCorner.CornerRadius = UDim.new(0, 8)
     uiCorner.Parent = toggleButton
 
-    local tabsEnabled = true  -- Initial state, tabs are enabled
+    local tabsEnabled = true  
 
     toggleButton.MouseButton1Click:Connect(function()
         tabsEnabled = not tabsEnabled
@@ -145,7 +133,7 @@ function CreateTabToggle(params)
         toggleButton.TextColor3 = tabsEnabled and Color3.new(1, 1, 1) or Color3.new(0.5, 0, 0.8)
     end)
 
-    toggleButton.Visible = true  -- Ensure the toggle is initially visible
+    toggleButton.Visible = true  
 
     return toggleButton
 end
